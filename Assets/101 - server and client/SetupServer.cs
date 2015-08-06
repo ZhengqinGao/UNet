5ccedb34fc62d0fbf85eb67360ee67d3a8c52700
Rocky Lai <rockylai@qq.com>
2015-08-06 16:33:53 +0800

    init commit

---
+++ Assets/101 - server and client/SetupServer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupServer : MonoBehaviour
+{
+    public int listenPort = 4444;
+
+    void Start()
+    {
+        ServerSetup();
+    }
+
+    private void ServerSetup()
+    {
+        NetworkServer.Listen(listenPort);
+        NetworkServer.RegisterHandler(MsgType.Connect, OnConnection);
+    }
+    
+    private void OnConnection(NetworkMessage msg)
+    {
+        Debug.Log("Server: connection happen");
+    }
+}
