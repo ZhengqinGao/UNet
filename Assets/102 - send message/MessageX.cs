379f27cb256dc83a0194db3cac43584b41b2f1cd
Rocky Lai <rockylai@qq.com>
2015-08-07 13:59:30 +0800

    102 send mssage sample

---
+++ Assets/102 - send message/MessageX.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.Networking;
+
+// Customize message and Type
+//
+public class MessageX : MessageBase
+{
+    public static readonly short MsgType = short.MaxValue;
+
+    public string Message { get; set; }
+    public string From { get; set; }
+
+    public override string ToString()
+    {
+        return string.Format("Message '{0}' from '{1}'", Message, From);
+    }
+}
---
+++ Assets/102 - send message/SendMessageX.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class SendMessageX : MonoBehaviour
+{
+    public int serverPort = 4444;
+    public InputField messageInput;
+    public InputField fromInput;
+
+    void Start()
+    {
+        SetupServer();
+        SetupClient();
+    }
+
+    #region Server 
+
+    private void SetupServer()
+    {
+        if (NetworkServer.active)
+        {
+            return;
+        }
+
+        NetworkServer.RegisterHandler(MsgType.Connect, OnConnected);
+        NetworkServer.RegisterHandler(MessageX.MsgType, OnMessageXReceived);
+
+        bool success = NetworkServer.Listen(serverPort);
+
+        if (success)
+        {
+            Debug.Log("Server Started");
+        }
+        else
+        {
+            Debug.Log("Start Server failed");
+        }
+    }
+
+    private void OnConnected(NetworkMessage msg)
+    {
+        Debug.Log("A client connected!");
+    }
+
+    private void OnMessageXReceived(NetworkMessage msg)
+    {
+        MessageX mx = msg.ReadMessage<MessageX>();
+        Debug.Log(string.Format("SERVER: {0}", mx));
+    }
+
+    #endregion
+
+    #region Client
+
+    NetworkClient client;
+
+    private void SetupClient()
+    {
+        if (client == null)
+        {
+            client = new NetworkClient();
+            client.Connect("127.0.0.1", serverPort);
+        }
+    }
+
+    public void SendMesssageXToServer()
+    {
+        MessageX mx = new MessageX();
+        mx.Message = messageInput.text;
+        mx.From = fromInput.text;
+
+        client.Send(MessageX.MsgType, mx);
+    }
+
+    #endregion
+}
