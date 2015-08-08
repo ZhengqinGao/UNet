a9cd727e06e039e15d9aaecedd3bd5be6da4e748
Rocky Lai <rockylai@qq.com>
2015-08-08 16:47:44 +0800

    add 103 introduce NetworkManager

---
+++ Assets/103 - introduce NetworkManager/ComBaseOnNetworkManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class ComBaseOnNetworkManager : MonoBehaviour {
+
+	private NetworkClient client;
+
+	private float interval = 5f;
+	private float time = 0f;
+	
+	void Start () {
+		SetupServer();
+		SetupClient();
+	}
+	
+	void Update()
+	{
+		time += Time.deltaTime;
+		if(time >= interval)
+		{
+			time = time - interval;
+			SendMessageToServer();
+		}
+	}
+	
+	private void SetupServer()
+	{
+		if(NetworkServer.active)
+		{
+			return;
+		}
+		
+		NetworkServer.RegisterHandler(MessageX.MsgType, OnMessageReceived);
+		
+		NetworkManager.singleton.StartServer();
+	}
+	
+	private void SetupClient()
+	{
+		client = NetworkManager.singleton.StartClient();
+	}
+	
+	private void OnMessageReceived(NetworkMessage msg)
+	{
+		Debug.Log(string.Format("SERVER: {0}", msg.ReadMessage<MessageX>()));
+	}
+	
+	private void SendMessageToServer()
+	{
+		MessageX mx = new MessageX();
+		mx.From = "NetworkMangerBase Sample";
+		mx.Message = "Hello Rocky!";
+		
+		client.Send(MessageX.MsgType, mx);
+	}
+}
