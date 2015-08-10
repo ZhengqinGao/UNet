89fd06a0d2b9700d1cdb51753a3073ccfadc23a6
Rocky Lai <rockylai@qq.com>
2015-08-10 23:05:18 +0800

    add SyncVar sample

---
+++ Assets/104 - syncvar/SimpleNetworkGUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class SimpleNetworkGUI : MonoBehaviour
+{
+	bool isHaveNetworkRole = false;
+	
+	void Start()
+	{
+		isHaveNetworkRole = false;
+	}
+	
+    void OnGUI()
+    {
+		if(isHaveNetworkRole)
+		{
+			return;
+		}
+		
+		if(GUI.Button(new Rect(Screen.width / 2f - 80, Screen.height / 2 - 12, 160, 24), "Start Host"))
+		{
+			NetworkManager.singleton.StartHost();
+			isHaveNetworkRole = true;
+		}
+		
+		if(GUI.Button(new Rect(Screen.width / 2f - 80, Screen.height / 2 + 24, 160, 24), "Start Client"))
+		{
+			NetworkManager.singleton.StartClient();
+			isHaveNetworkRole = true;
+		}
+		
+	}
+}
---
+++ Assets/104 - syncvar/SyncVarSample.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+
+public class SyncVarSample : NetworkBehaviour
+{
+
+    [SyncVar]
+    string State;
+
+    void Start()
+    {
+		Init();
+    }
+
+    private void Init()
+    {
+        State = "Init State";
+    }
+
+	private void ChangeState()
+	{
+		State = "State Changed";
+	}
+
+	void OnGUI()
+	{
+		GUI.Label(new Rect(10, 20, 120, 24), State);
+		
+		if(isServer)
+		{
+			if(GUI.Button(new Rect(10, 56, 160, 24), "ChangeState"))
+			{
+				ChangeState();
+			}
+		}
+	}
+}
