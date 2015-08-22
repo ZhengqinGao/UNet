a3464bece30ff6c7e92e0e336d739a58b6e40ff6
Rocky Lai <rockylai@qq.com>
2015-08-22 20:42:04 +0800

    command sample

---
+++ Assets/106 - Command/CommandSample.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class CommandSample : NetworkBehaviour {
+
+	[SyncVar]
+    string State = "Init State";
+
+    private void ChangeState()
+    {
+        State = Random.Range(0, int.MaxValue).ToString();
+    }
+    
+    [Command]
+    private void CmdChangeState()
+    {
+        ChangeState();
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 20, 120, 24), State);
+        
+        if (!isServer)
+        {
+            if (GUI.Button(new Rect(10, 56, 160, 24), "ChangeState"))
+            {
+                //  ChangeState();
+                CmdChangeState();
+            }
+        }
+    }
+}
