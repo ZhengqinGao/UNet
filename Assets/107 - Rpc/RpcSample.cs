aa1df565e69952c1298b547b0535c4d5bed4d0d7
Rocky Lai <rockylai@qq.com>
2015-08-24 22:34:55 +0800

    ClientRPC

---
+++ Assets/107 - Rpc/RpcSample.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class RpcSample : NetworkBehaviour {
+
+    string State = "Init State";
+
+    private void ChangeState()
+    {
+        State = Random.Range(0, int.MaxValue).ToString();
+    }
+    
+	[ClientRpc]
+	private void RpcChangeState() 
+	{
+		ChangeState();
+	}
+	
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 20, 120, 24), State);
+        
+        if (isServer)
+        {
+            if (GUI.Button(new Rect(10, 56, 160, 24), "ChangeState"))
+            {
+                RpcChangeState();
+            }
+        }
+    }
+}
